15c3cd368df56fced4c8a59aca79741f6288d9cd
chriswlai <ch94518@gmail.com>
2024-10-30 11:24:01 -0700

    fixtures final

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,33 +1,33 @@
 const organizationFixtures = {
     oneOrg: [
         {
-            orgCode: "ACM",
-            orgTranslationShort: "ACM",
-            orgTranslation: "Association for Computing Machinery",
-            inactive: false
+            "orgCode": "ACM",
+            "orgTranslationShort": "ACM",
+            "orgTranslation": "Association for Computing Machinery",
+            "inactive": false
         },
     ],
 
     threeOrgs: [
         {
-            orgCode: "TASA",
-            orgTranslationShort: "TASA",
-            orgTranslation: "Taiwanese American Student Association",
-            inactive: false
+            "orgCode": "TASA",
+            "orgTranslationShort": "TASA",
+            "orgTranslation": "Taiwanese American Student Association",
+            "inactive": false
         },
 
         {
-            orgCode: "IEEE",
-            orgTranslationShort: "IEEE",
-            orgTranslation: "Institute of Electrical and Electronics Engineers",
-            inactive: false
+            "orgCode": "IEEE",
+            "orgTranslationShort": "IEEE",
+            "orgTranslation": "Institute of Electrical and Electronics Engineers",
+            "inactive": false
         },
 
         {
-            orgCode: "DSClub",
-            orgTranslationShort: "DSClub",
-            orgTranslation: "Data Science Club",
-            inactive: false
+            "orgCode": "DSClub",
+            "orgTranslationShort": "DSClub",
+            "orgTranslation": "Data Science Club",
+            "inactive": false
         },
     ],
 };
