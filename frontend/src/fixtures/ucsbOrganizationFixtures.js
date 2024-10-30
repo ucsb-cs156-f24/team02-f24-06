fa77998d028964a4fc45105e46184015ac7ea061
chriswlai <ch94518@gmail.com>
2024-10-30 11:36:34 -0700

    format

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,35 +1,35 @@
 const organizationFixtures = {
-    oneOrg: [
-        {
-            "orgCode": "ACM",
-            "orgTranslationShort": "ACM",
-            "orgTranslation": "Association for Computing Machinery",
-            "inactive": false
-        },
-    ],
-
-    threeOrgs: [
-        {
-            "orgCode": "TASA",
-            "orgTranslationShort": "TASA",
-            "orgTranslation": "Taiwanese American Student Association",
-            "inactive": false
-        },
-
-        {
-            "orgCode": "IEEE",
-            "orgTranslationShort": "IEEE",
-            "orgTranslation": "Institute of Electrical and Electronics Engineers",
-            "inactive": false
-        },
-
-        {
-            "orgCode": "DSClub",
-            "orgTranslationShort": "DSClub",
-            "orgTranslation": "Data Science Club",
-            "inactive": false
-        },
-    ],
+  oneOrg: [
+    {
+      orgCode: "ACM",
+      orgTranslationShort: "ACM",
+      orgTranslation: "Association for Computing Machinery",
+      inactive: false,
+    },
+  ],
+  
+  threeOrgs: [
+    {
+      orgCode: "TASA",
+      orgTranslationShort: "TASA",
+      orgTranslation: "Taiwanese American Student Association",
+      inactive: false,
+    },
+  
+    {
+      orgCode: "IEEE",
+      orgTranslationShort: "IEEE",
+      orgTranslation: "Institute of Electrical and Electronics Engineers",
+      inactive: false,
+    },
+  
+    {
+      orgCode: "DSClub",
+      orgTranslationShort: "DSClub",
+      orgTranslation: "Data Science Club",
+      inactive: false,
+    },
+  ],
 };
 
 export { organizationFixtures};
