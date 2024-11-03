312b0dfe6161168b636dd01eddef97a08b8babe4
chriswlai <ch94518@gmail.com>
2024-11-02 18:12:20 -0700

    prettier

--- frontend/src/stories/components/UCSBOrganization/UCSBOrganizationForm.stories.js
+++ frontend/src/stories/components/UCSBOrganization/UCSBOrganizationForm.stories.js
@@ -3,31 +3,31 @@ import UCSBOrganizationForm from "main/components/UCSBOrganization/UCSBOrganizat
 import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
 
 export default {
-    title: "components/UCSBOrganization/UCSBOrganizationForm",
-    component: UCSBOrganizationForm,
+  title: "components/UCSBOrganization/UCSBOrganizationForm",
+  component: UCSBOrganizationForm,
 };
 
 const Template = (args) => {
-    return <UCSBOrganizationForm {...args} />;
-}
+  return <UCSBOrganizationForm {...args} />;
+};
 
 export const Create = Template.bind({});
 
 Create.args = {
-    buttonLabel: "Create",
-    submitAction: (data) => {
-        console.log("Submit was clicked with data: ", data);
-        window.alert("Submit was clicked with data: " + JSON.stringify(data));
-    },
+  buttonLabel: "Create",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
 };
 
 export const Update = Template.bind({});
 
 Update.args = {
-    initialContents: ucsbOrganizationFixtures.oneOrg[0],
-    buttonLabel: "Update",
-    submitAction: (data) => {
-        console.log("Submit was clicked with data: ", data);
-        window.alert("Submit was clicked with data: " + JSON.stringify(data));
-    },
+  initialContents: ucsbOrganizationFixtures.oneOrg[0],
+  buttonLabel: "Update",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
 };
