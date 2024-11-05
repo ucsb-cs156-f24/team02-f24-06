b8dcadba889518fd488971265194b8913b572b36
Victor Prchlik <vprchlik@ucsb.edu>
2024-11-05 02:29:48 -0800

    vp - edited with prettier

--- frontend/src/main/components/Articles/ArticlesTable.js
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -2,68 +2,72 @@ import React from "react";
 import OurTable, { ButtonColumn } from "main/components/OurTable";
 
 import { useBackendMutation } from "main/utils/useBackend";
-import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/ArticlesUtils"
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/ArticlesUtils";
 import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
 export default function ArticlesTable({ articles, currentUser }) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const editCallback = (cell) => {
+    navigate(`/articles/edit/${cell.row.values.id}`);
+  };
 
-    const editCallback = (cell) => {
-        navigate(`/articles/edit/${cell.row.values.id}`)
-    }
+  // Stryker disable all : hard to test for query caching
 
-    // Stryker disable all : hard to test for query caching
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
 
-    const deleteMutation = useBackendMutation(
-        cellToAxiosParamsDelete,
-        { onSuccess: onDeleteSuccess },
+    ["/api/articles/all"],
+  );
+  // Stryker restore all
 
-        ["/api/articles/all"]
+  // Stryker disable next-line all : TODO try to make a good test for this
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
 
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id",
+    },
+    {
+      Header: "Title",
+      accessor: "title",
+    },
+    {
+      Header: "URL",
+      accessor: "url",
+    },
+    {
+      Header: "Explanation",
+      accessor: "explanation",
+    },
+    {
+      Header: "Email",
+      accessor: "email",
+    },
+    {
+      Header: "dateAdded",
+      accessor: "dateAdded",
+    },
+  ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(
+      ButtonColumn("Edit", "primary", editCallback, "ArticlesTable"),
     );
-    // Stryker restore all 
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, "ArticlesTable"),
+    );
+  }
 
-    // Stryker disable next-line all : TODO try to make a good test for this
-    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
-
-
-    const columns = [
-        {
-            Header: 'id',
-            accessor: 'id',
-        },
-        {
-            Header: 'Title',
-            accessor: 'title',
-        },
-        {
-            Header: 'URL',
-            accessor: 'url',
-        },
-        {
-            Header: 'Explanation',
-            accessor: 'explanation',
-        },
-        {
-            Header: 'Email',
-            accessor: 'email',
-        },
-        {
-            Header: 'dateAdded',
-            accessor: 'dateAdded',
-        }
-    ];
-
-    if (hasRole(currentUser, "ROLE_ADMIN")) {
-        columns.push(ButtonColumn("Edit", "primary", editCallback, "ArticlesTable"));
-        columns.push(ButtonColumn("Delete", "danger", deleteCallback, "ArticlesTable"));
-    } 
-
-    return <OurTable
-        data={articles}
-        columns={columns}
-        testid={"ArticlesTable"}
-    />;
-};
+  return (
+    <OurTable data={articles} columns={columns} testid={"ArticlesTable"} />
+  );
+}
--- frontend/src/tests/components/Articles/ArticlesTable.test.js
+++ frontend/src/tests/components/Articles/ArticlesTable.test.js
@@ -7,28 +7,44 @@ import { currentUserFixtures } from "fixtures/currentUserFixtures";
 
 const mockedNavigate = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockedNavigate
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
 }));
 
 describe("UserTable tests", () => {
   const queryClient = new QueryClient();
 
   test("Has the expected column headers and content for ordinary user", () => {
-
     const currentUser = currentUserFixtures.userOnly;
 
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
-          <ArticlesTable articles={articlesFixtures.threeArticles} currentUser={currentUser} />
+          <ArticlesTable
+            articles={articlesFixtures.threeArticles}
+            currentUser={currentUser}
+          />
         </MemoryRouter>
-      </QueryClientProvider>
+      </QueryClientProvider>,
     );
 
-    const expectedHeaders = ["id", "Title", "URL", "Explanation", "Email", "dateAdded"];
-    const expectedFields = ["id", "title", "url", "explanation", "email", "dateAdded"];
+    const expectedHeaders = [
+      "id",
+      "Title",
+      "URL",
+      "Explanation",
+      "Email",
+      "dateAdded",
+    ];
+    const expectedFields = [
+      "id",
+      "title",
+      "url",
+      "explanation",
+      "email",
+      "dateAdded",
+    ];
     const testId = "ArticlesTable";
 
     expectedHeaders.forEach((headerText) => {
@@ -41,31 +57,71 @@ describe("UserTable tests", () => {
       expect(header).toBeInTheDocument();
     });
 
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-title`)).toHaveTextContent("Sample Article 1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-url`)).toHaveTextContent("https://samplewebsite.com/article1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-explanation`)).toHaveTextContent("Explanation for sample article 1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-email`)).toHaveTextContent("sample1@ucsb.edu");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-dateAdded`)).toHaveTextContent("2022-04-20T12:00:00");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
+      "1",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-title`),
+    ).toHaveTextContent("Sample Article 1");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-url`),
+    ).toHaveTextContent("https://samplewebsite.com/article1");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-explanation`),
+    ).toHaveTextContent("Explanation for sample article 1");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-email`),
+    ).toHaveTextContent("sample1@ucsb.edu");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-dateAdded`),
+    ).toHaveTextContent("2022-04-20T12:00:00");
 
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-title`)).toHaveTextContent("Sample Article 2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-url`)).toHaveTextContent("https://samplewebsite.com/article2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-explanation`)).toHaveTextContent("Explanation for sample article 2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-email`)).toHaveTextContent("sample2@ucsb.edu");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-dateAdded`)).toHaveTextContent("2022-04-19T12:00:00");
+    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
+      "2",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-title`),
+    ).toHaveTextContent("Sample Article 2");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-url`),
+    ).toHaveTextContent("https://samplewebsite.com/article2");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-explanation`),
+    ).toHaveTextContent("Explanation for sample article 2");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-email`),
+    ).toHaveTextContent("sample2@ucsb.edu");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-dateAdded`),
+    ).toHaveTextContent("2022-04-19T12:00:00");
 
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-title`)).toHaveTextContent("Sample Article 3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-url`)).toHaveTextContent("https://samplewebsite.com/article3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-explanation`)).toHaveTextContent("Explanation for sample article 3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-email`)).toHaveTextContent("sample3@ucsb.edu");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-dateAdded`)).toHaveTextContent("2022-04-20T12:00:00");
+    expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent(
+      "3",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-title`),
+    ).toHaveTextContent("Sample Article 3");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-url`),
+    ).toHaveTextContent("https://samplewebsite.com/article3");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-explanation`),
+    ).toHaveTextContent("Explanation for sample article 3");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-email`),
+    ).toHaveTextContent("sample3@ucsb.edu");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-dateAdded`),
+    ).toHaveTextContent("2022-04-20T12:00:00");
 
-    const editButton = screen.queryByTestId(`${testId}-cell-row-0-col-Edit-button`);
+    const editButton = screen.queryByTestId(
+      `${testId}-cell-row-0-col-Edit-button`,
+    );
     expect(editButton).not.toBeInTheDocument();
 
-    const deleteButton = screen.queryByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    const deleteButton = screen.queryByTestId(
+      `${testId}-cell-row-0-col-Delete-button`,
+    );
     expect(deleteButton).not.toBeInTheDocument();
   });
 
@@ -75,13 +131,30 @@ describe("UserTable tests", () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
-          <ArticlesTable articles={articlesFixtures.threeArticles} currentUser={currentUser} />
+          <ArticlesTable
+            articles={articlesFixtures.threeArticles}
+            currentUser={currentUser}
+          />
         </MemoryRouter>
-      </QueryClientProvider>
+      </QueryClientProvider>,
     );
 
-    const expectedHeaders = ["id", "Title", "URL", "Explanation", "Email", "dateAdded"];
-    const expectedFields = ["id", "title", "url", "explanation", "email", "dateAdded"];
+    const expectedHeaders = [
+      "id",
+      "Title",
+      "URL",
+      "Explanation",
+      "Email",
+      "dateAdded",
+    ];
+    const expectedFields = [
+      "id",
+      "title",
+      "url",
+      "explanation",
+      "email",
+      "dateAdded",
+    ];
     const testId = "ArticlesTable";
 
     expectedHeaders.forEach((headerText) => {
@@ -94,32 +167,72 @@ describe("UserTable tests", () => {
       expect(header).toBeInTheDocument();
     });
 
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-title`)).toHaveTextContent("Sample Article 1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-url`)).toHaveTextContent("https://samplewebsite.com/article1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-explanation`)).toHaveTextContent("Explanation for sample article 1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-email`)).toHaveTextContent("sample1@ucsb.edu");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-dateAdded`)).toHaveTextContent("2022-04-20T12:00:00");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
+      "1",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-title`),
+    ).toHaveTextContent("Sample Article 1");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-url`),
+    ).toHaveTextContent("https://samplewebsite.com/article1");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-explanation`),
+    ).toHaveTextContent("Explanation for sample article 1");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-email`),
+    ).toHaveTextContent("sample1@ucsb.edu");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-dateAdded`),
+    ).toHaveTextContent("2022-04-20T12:00:00");
 
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-title`)).toHaveTextContent("Sample Article 2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-url`)).toHaveTextContent("https://samplewebsite.com/article2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-explanation`)).toHaveTextContent("Explanation for sample article 2");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-email`)).toHaveTextContent("sample2@ucsb.edu");
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-dateAdded`)).toHaveTextContent("2022-04-19T12:00:00");
+    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
+      "2",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-title`),
+    ).toHaveTextContent("Sample Article 2");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-url`),
+    ).toHaveTextContent("https://samplewebsite.com/article2");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-explanation`),
+    ).toHaveTextContent("Explanation for sample article 2");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-email`),
+    ).toHaveTextContent("sample2@ucsb.edu");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-1-col-dateAdded`),
+    ).toHaveTextContent("2022-04-19T12:00:00");
 
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-title`)).toHaveTextContent("Sample Article 3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-url`)).toHaveTextContent("https://samplewebsite.com/article3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-explanation`)).toHaveTextContent("Explanation for sample article 3");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-email`)).toHaveTextContent("sample3@ucsb.edu");
-    expect(screen.getByTestId(`${testId}-cell-row-2-col-dateAdded`)).toHaveTextContent("2022-04-20T12:00:00");
+    expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent(
+      "3",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-title`),
+    ).toHaveTextContent("Sample Article 3");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-url`),
+    ).toHaveTextContent("https://samplewebsite.com/article3");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-explanation`),
+    ).toHaveTextContent("Explanation for sample article 3");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-email`),
+    ).toHaveTextContent("sample3@ucsb.edu");
+    expect(
+      screen.getByTestId(`${testId}-cell-row-2-col-dateAdded`),
+    ).toHaveTextContent("2022-04-20T12:00:00");
 
-    const editButton = screen.getByTestId(`${testId}-cell-row-0-col-Edit-button`);
+    const editButton = screen.getByTestId(
+      `${testId}-cell-row-0-col-Edit-button`,
+    );
     expect(editButton).toBeInTheDocument();
     expect(editButton).toHaveClass("btn-primary");
 
-    const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    const deleteButton = screen.getByTestId(
+      `${testId}-cell-row-0-col-Delete-button`,
+    );
     expect(deleteButton).toBeInTheDocument();
     expect(deleteButton).toHaveClass("btn-danger");
   });
@@ -131,20 +244,31 @@ describe("UserTable tests", () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
-          <ArticlesTable articles={articlesFixtures.threeArticles} currentUser={currentUser} />
+          <ArticlesTable
+            articles={articlesFixtures.threeArticles}
+            currentUser={currentUser}
+          />
         </MemoryRouter>
-      </QueryClientProvider>
+      </QueryClientProvider>,
     );
 
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-title`)).toHaveTextContent("Sample Article 1");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
+      "1",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-title`),
+    ).toHaveTextContent("Sample Article 1");
 
-    const editButton = screen.getByTestId(`${testId}-cell-row-0-col-Edit-button`);
+    const editButton = screen.getByTestId(
+      `${testId}-cell-row-0-col-Edit-button`,
+    );
     expect(editButton).toBeInTheDocument();
 
     fireEvent.click(editButton);
 
-    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/articles/edit/1'));
+    await waitFor(() =>
+      expect(mockedNavigate).toHaveBeenCalledWith("/articles/edit/1"),
+    );
   });
 
   test("Delete button navigates to the delete page", async () => {
@@ -154,15 +278,24 @@ describe("UserTable tests", () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
-          <ArticlesTable articles={articlesFixtures.threeArticles} currentUser={currentUser} />
+          <ArticlesTable
+            articles={articlesFixtures.threeArticles}
+            currentUser={currentUser}
+          />
         </MemoryRouter>
-      </QueryClientProvider>
+      </QueryClientProvider>,
     );
 
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-title`)).toHaveTextContent("Sample Article 1");
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
+      "1",
+    );
+    expect(
+      screen.getByTestId(`${testId}-cell-row-0-col-title`),
+    ).toHaveTextContent("Sample Article 1");
 
-    const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    const deleteButton = screen.getByTestId(
+      `${testId}-cell-row-0-col-Delete-button`,
+    );
     expect(deleteButton).toBeInTheDocument();
 
     fireEvent.click(deleteButton);
