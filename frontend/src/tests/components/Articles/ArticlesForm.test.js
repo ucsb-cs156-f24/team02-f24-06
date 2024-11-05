595b736e2d431191c435b661b66a8236a3dd7310
Victor Prchlik <vprchlik@ucsb.edu>
2024-11-04 16:41:37 -0800

    vp - another fix attempt tests

--- frontend/src/tests/components/Articles/ArticlesForm.test.js
+++ frontend/src/tests/components/Articles/ArticlesForm.test.js
@@ -1,9 +1,7 @@
 import { render, waitFor, fireEvent, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-
 import ArticlesForm from "main/components/Articles/ArticlesForm";
 import { articlesFixtures } from "fixtures/articlesFixtures";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const mockedNavigate = jest.fn();
@@ -16,30 +14,16 @@ jest.mock("react-router-dom", () => ({
 describe("ArticlesForm tests", () => {
   const queryClient = new QueryClient();
 
-  const expectedHeaders = [
-    "Title",
-    "URL",
-    "Explanation",
-    "Email",
-    "Date Added (iso format)",
-  ];
-  const testId = "ArticlesForm";
-
   test("renders correctly with no initialContents", async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <Router>
           <ArticlesForm />
         </Router>
-      </QueryClientProvider>,
+      </QueryClientProvider>
     );
-
-    expect(await screen.findByText(/Create/)).toBeInTheDocument();
-
-    expectedHeaders.forEach((headerText) => {
-      const header = screen.getByText(headerText);
-      expect(header).toBeInTheDocument();
-    });
+    await screen.findByText(/Title/);
+    await screen.findByText(/Create/);
   });
 
   test("renders correctly when passing in initialContents", async () => {
@@ -48,18 +32,84 @@ describe("ArticlesForm tests", () => {
         <Router>
           <ArticlesForm initialContents={articlesFixtures.oneArticle} />
         </Router>
-      </QueryClientProvider>,
+      </QueryClientProvider>
+    );
+    await screen.findByTestId("ArticlesForm-id");
+    expect(screen.getByText(/Id/)).toBeInTheDocument();
+    expect(screen.getByTestId("ArticlesForm-id")).toHaveValue("1");
+  });
+
+  test("Correct error messages on bad input", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <ArticlesForm />
+        </Router>
+      </QueryClientProvider>
+    );
+    await screen.findByTestId("ArticlesForm-url");
+    const urlField = screen.getByTestId("ArticlesForm-url");
+    const emailField = screen.getByTestId("ArticlesForm-email");
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+    fireEvent.change(urlField, { target: { value: "invalid-url" } });
+    fireEvent.change(emailField, { target: { value: "invalid-email" } });
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/URL must be valid/);
+    await screen.findByText(/Email must be valid/);
+  });
+
+  test("Correct error messages on missing input", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <ArticlesForm />
+        </Router>
+      </QueryClientProvider>
+    );
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Title is required/);
+    await screen.findByText(/URL is required/);
+    await screen.findByText(/Explanation is required/);
+    await screen.findByText(/Email is required/);
+    await screen.findByText(/Date added is required/);
+  });
+
+  test("No error messages on good input", async () => {
+    const mockSubmitAction = jest.fn();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <ArticlesForm submitAction={mockSubmitAction} />
+        </Router>
+      </QueryClientProvider>
     );
 
-    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    const titleField = screen.getByTestId("ArticlesForm-title");
+    const urlField = screen.getByTestId("ArticlesForm-url");
+    const explanationField = screen.getByTestId("ArticlesForm-explanation");
+    const emailField = screen.getByTestId("ArticlesForm-email");
+    const dateAddedField = screen.getByTestId("ArticlesForm-dateAdded");
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
 
-    expectedHeaders.forEach((headerText) => {
-      const header = screen.getByText(headerText);
-      expect(header).toBeInTheDocument();
-    });
+    fireEvent.change(titleField, { target: { value: "Sample Article" } });
+    fireEvent.change(urlField, { target: { value: "http://example.com" } });
+    fireEvent.change(explanationField, { target: { value: "Sample explanation" } });
+    fireEvent.change(emailField, { target: { value: "test@example.com" } });
+    fireEvent.change(dateAddedField, { target: { value: "2022-01-01T12:00" } });
+    fireEvent.click(submitButton);
 
-    expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
-    expect(screen.getByText("Id")).toBeInTheDocument();
+    await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+    expect(screen.queryByText(/Title is required/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/URL is required/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Explanation is required/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Email is required/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Date added is required/)).not.toBeInTheDocument();
   });
 
   test("that navigate(-1) is called when Cancel is clicked", async () => {
@@ -68,84 +118,10 @@ describe("ArticlesForm tests", () => {
         <Router>
           <ArticlesForm />
         </Router>
-      </QueryClientProvider>,
+      </QueryClientProvider>
     );
-    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
-    const cancelButton = screen.getByTestId(`${testId}-cancel`);
-
+    const cancelButton = screen.getByTestId("ArticlesForm-cancel");
     fireEvent.click(cancelButton);
-
     await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
-  });
-
-  test("that the correct validations are performed", async () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <ArticlesForm />
-        </Router>
-      </QueryClientProvider>,
-    );
-
-    expect(await screen.findByText(/Create/)).toBeInTheDocument();
-    const submitButton = screen.getByText(/Create/);
-    fireEvent.click(submitButton);
-
-    await screen.findByText(/Title is required/);
-    expect(screen.getByText(/URL is required/)).toBeInTheDocument();
-    expect(screen.getByText(/Explanation is required/)).toBeInTheDocument();
-    expect(screen.getByText(/Email is required/)).toBeInTheDocument();
-
-    // Check for the presence of "Date added is required"
-    expect(screen.queryByText(/Date added is required/)).toBeInTheDocument();
-
-    const titleInput = screen.getByTestId(`${testId}-title`);
-    fireEvent.change(titleInput, { target: { value: "a".repeat(256) } });
-    fireEvent.click(submitButton);
-
-    await waitFor(() => {
-      expect(screen.getByText(/Max length 255 characters/)).toBeInTheDocument();
-    });
-  });
-
-  test("No Error messages on valid input", async () => {
-    const mockSubmitAction = jest.fn();
-
-    render(
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <ArticlesForm submitAction={mockSubmitAction} />
-        </Router>
-      </QueryClientProvider>,
-    );
-
-    const titleField = screen.getByTestId(`${testId}-title`);
-    const urlField = screen.getByTestId(`${testId}-url`);
-    const explanationField = screen.getByTestId(`${testId}-explanation`);
-    const emailField = screen.getByTestId(`${testId}-email`);
-    const dateAddedField = screen.getByTestId(`${testId}-dateAdded`);
-    const submitButton = screen.getByTestId(`${testId}-submit`);
-
-    fireEvent.change(titleField, { target: { value: "Sample Article" } });
-    fireEvent.change(urlField, { target: { value: "http://example.com" } });
-    fireEvent.change(explanationField, {
-      target: { value: "Sample explanation" },
-    });
-    fireEvent.change(emailField, { target: { value: "test@example.com" } });
-    fireEvent.change(dateAddedField, { target: { value: "2022-01-01T12:00" } });
-
-    fireEvent.click(submitButton);
-
-    await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
-
-    expect(screen.queryByText(/Title is required/)).not.toBeInTheDocument();
-    expect(screen.queryByText(/URL is required/)).not.toBeInTheDocument();
-    expect(
-      screen.queryByText(/Explanation is required/),
-    ).not.toBeInTheDocument();
-    expect(screen.queryByText(/Email is required/)).not.toBeInTheDocument();
-    expect(
-      screen.queryByText(/Date added is required/),
-    ).not.toBeInTheDocument();
   });
 });
